4860239a17ab810576b1a8ebb0214542507eb3cf
binal.patel <binal.patel.sa@gmail.com>
2020-06-01 19:09:21 +0530

    done about page and menu page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,25 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
+import { MenuComponent } from './menu/menu.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "", redirectTo: "/home", pathMatch: "full" },
+  {
+    path: 'home',
+    component: HomeComponent
+  },
+  {
+    path: 'about',
+    component: AboutComponent
+  },
+  {
+    path: 'menu',
+    component: MenuComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
